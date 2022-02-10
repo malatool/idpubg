fe02f1de0a9562ba8a6995edaf134c3cd599edfd
malatool <99417939+malatool@users.noreply.github.com>
2022-02-10 21:12:12 +0300

    Create idpubg.py

---
+++ idpubg.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import time
+import requests
+ID = raw_input('ID PUBG : ')
+os.system('clear')
+print 'YOUR ID ' + ID
+logo = 'Cod By Palabunii'
+print logo
+print '\x1b[33;1mi had played with hacker'
+print 'I used hacks'
+print ' I used fake uc'
+print ' i have banded because of virtual'
+print ' labar hokareki tr xot ba englizi binusa !'
+print ' \x1b[5;37;40m-------------------------------'
+print 'abe nusinaka hamuy bnusyawa ba juani !!'
+CLAY = raw_input('bo band buit ? :')
+os.system('spam dasy pe krd bo am id ya' + ID + CLAY)
